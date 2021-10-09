67cb808f534df006f1bc05b3bb76d3e33aa7fda1
Omegaviv <86337946+Omegaviv@users.noreply.github.com>
2021-10-09 19:31:36 +0530

    Update WDtemp1.php

--- WDtemp1.php
+++ WDtemp1.php
@@ -5,7 +5,8 @@ if(isset($_POST['name']) && isset($_POST['email'])){
     $name = $_POST['name'];
     $email = $_POST['email'];
     $subject = $_POST['subject'];
-    $body = $_POST['body'];
+    $messge = $_POST['message'];
+    
 
     require_once "PHPMailer/PHPMailer.php";
     require_once "PHPMailer/SMTP.php";
@@ -17,15 +18,15 @@ if(isset($_POST['name']) && isset($_POST['email'])){
     $mail->isSMTP();
     $mail->Host = "smtp.gmail.com";
     $mail->SMTPAuth = true;
-    $mail->Username = "youremail@gmail.com";
-    $mail->Password = 'yourpassword';
+    $mail->Username = "workdone2002@gmail.com";
+    $mail->Password = 'workdone@2002';
     $mail->Port = 465;
     $mail->SMTPSecure = "ssl";
 
     //email settings
     $mail->isHTML(true);
     $mail->setFrom($email, $name);
-    $mail->addAddress("youremail@gmail.com");
+    $mail->addAddress("workdone2002@gmail.com");
     $mail->Subject = ("$email ($subject)");
     $mail->Body = $body;
 
@@ -36,7 +37,7 @@ if(isset($_POST['name']) && isset($_POST['email'])){
     else
     {
         $status = "failed";
-        $response = "Something is wrong: <br>" . $mail->ErrorInfo;
+        $response = "Something went wrong: <br>" . $mail->ErrorInfo;
     }
 
     exit(json_encode(array("status" => $status, "response" => $response)));
